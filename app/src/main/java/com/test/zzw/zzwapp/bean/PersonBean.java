0bdaa041ec58418cd510cf88e66ac9cf32cdcd70
zzw <464728422@qq.com>
2017-04-19 16:18:46 +0800

    170419_ver4

---
+++ app/src/main/java/com/test/zzw/zzwapp/bean/PersonBean.java
@@ -0,0 +1,35 @@
+package com.test.zzw.zzwapp.bean;
+
+/**
+ * Created by zzw on 2017/4/19.
+ */
+
+public class PersonBean {
+    private int id;
+    private String name;
+    private String addr;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+}
